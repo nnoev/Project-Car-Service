dbd3f500875eee6e91f14d08c771cb0c346228ea
NikolayNoev <n.p.noev@gmail.com>
2026-08-17 18:04:17 +0300

    test: configure application context test

--- src/main/java/com/example/car_service/admin/AdminService.java
+++ src/main/java/com/example/car_service/admin/AdminService.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
 @Slf4j
 @Service
 public class AdminService {
