4686c5c26ad53d8e279d3d7e2078e78b21bfad61
NikolayNoev <n.p.noev@gmail.com>
2026-08-18 11:28:55 +0300

    refactor: extract service record microservice

--- car-service-microservice/src/main/java/com/example/car_service_microservice/service_record/exceptions/ServiceRecordNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.car_service_microservice.service_record.exceptions;
-
-public class ServiceRecordNotFoundException extends RuntimeException {
-
-    public ServiceRecordNotFoundException(String message) {
-        super(message);
-    }
-
-}
